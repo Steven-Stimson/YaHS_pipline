e503286bc59e554693627447caedee43b27fb056
Steven-Stimson <142574379+Steven-Stimson@users.noreply.github.com>
2024-12-02 21:54:03 +0800

    Add files via upload

---
+++ 01.step01.index_align.sh
@@ -0,0 +1,34 @@
+# Index and Alignment 
+export PATH=/opt/software/miniconda3/envs/samtools/bin:$PATH
+export PATH=/opt/software/miniconda3/envs/chromap/bin:$PATH
+
+EntityID=$1
+Threads=$2
+Contigs=$3
+HiC_R1=$4
+HiC_R2=$5
+
+ln -sf $Contigs ${EntityID}.fa
+samtools faidx ${EntityID}.fa
+chromap -i -r ${EntityID}.fa -o ${EntityID}.index 2>&1 | perl -ne '(/number of bases: (\d+)\.$/) && (print "assembly $1\n")'> ${EntityID}.chrom.sizes
+
+echo "Index Done."
+
+chromap --preset hic -r ${EntityID}.fa -x ${EntityID}.index \
+-t 20 --SAM -o aligned.sam \
+-1 ${HiC_R1} \
+-2 ${HiC_R2}
+
+echo "Alilgn Done."
+
+samtools view -@ 20 -b aligned.sam | samtools sort -@ 20 -o aligned.bam
+samtools view -@ 20 -b -F 4 -F 2048 aligned.bam -o aligned.pair.bam
+
+echo "Filter Done."
+
+export PATH=/opt/software/miniconda3/envs/java/bin:$PATH
+java -jar /opt/software/picard/picard.jar MarkDuplicates -I aligned.pair.bam -O ${SampleID}.hicaln.dedup.bam -REMOVE_DUPLICATES true -M dedup_metrics.txt
+
+echo "Dedup Done."
+
+echo "Successful."
---
+++ 02.step02.yahs_pipline.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+YaHS_DIR="/opt/software/yahs"
+TEST_OUTDIR="YaHS_result"
+mkdir -p ${TEST_OUTDIR}
+
+################################ START OF YAHS SCAFFOLDING TEST ################################
+#### download the EntityID data
+EntityID=$1
+out="${EntityID}_out"
+contigs="${EntityID}.fa" # need to be indexed, i.e., ${EntityID}.contigs.fasta.gz.fai is presented
+index="${EntityID}.fa.fai"
+hicaln="${EntityID}.hicaln.dedup.bam" # could be .bed, .bam or .bin file
+Restriction_site=$2
+juicer_tools="/opt/software/juicer_tools/juicer_tools_1.19.02.jar pre"
+
+#### run yahs scaffolding
+if [ -z "$3" ]; then
+(${YaHS_DIR}/yahs --file-type BAM -o ${TEST_OUTDIR}/${out} ${contigs} ${hicaln} > ${TEST_OUTDIR}/01.YaHS_scaffolding.log 2>&1) && (echo "YaHS scaffolding DONE.")
+else 
+(${YaHS_DIR}/yahs -e ${Restriction_site} --file-type BAM -o ${TEST_OUTDIR}/${out} ${contigs} ${hicaln} > ${TEST_OUTDIR}/01.YaHS_scaffolding.log 2>&1) && (echo "YaHS scaffolding DONE.")
+fi
+
+rm -f ${TEST_OUTDIR}/${out}_inital_break* &  rm -f ${TEST_OUTDIR}/${out}_r*
+
+(${YaHS_DIR}/agp_to_fasta ${TEST_OUTDIR}/${out}_scaffolds_final.agp ${contigs} -o ${TEST_OUTDIR}/${out}.fa >${TEST_OUTDIR}/02.AGPtoFASTA.log 2>&1) && (echo "AGP to FASTA DONE.")
+
+(${YaHS_DIR}/juicer pre ${TEST_OUTDIR}/${out}.bin ${TEST_OUTDIR}/${out}_scaffolds_final.agp $index >${TEST_OUTDIR}/${out}.aln.txt 2>${TEST_OUTDIR}/03.Juicer_pre.log) && (echo "Juicer pre DONE.")
+
+(${YaHS_DIR}/juicer pre ${TEST_OUTDIR}/${out}.bin ${TEST_OUTDIR}/${out}_scaffolds_final.agp $index -a -o ${TEST_OUTDIR}/${out}.JBAT >${TEST_OUTDIR}/04.Juicer_pre-a.log 2>&1 ) && (echo "Juicer pre -a DONE.")
+
+source /opt/software/miniconda3/bin/activate java
+(java -jar ${juicer_tools} ${TEST_OUTDIR}/${out}.JBAT.txt ${TEST_OUTDIR}/${out}.JBAT.hic <(cat ${EntityID}.chrom.sizes) >${TEST_OUTDIR}/05.Generate_.hic_file.log 2>&1) && (echo "Generate .hic file Done.")
+
+echo "Successful."
+
+################################# END OF YAHS SCAFFOLDING TEST #################################

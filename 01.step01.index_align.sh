9abcacc1898228d68ff75ef44b6c352eaf60fabb
Steven-Stimson <142574379+Steven-Stimson@users.noreply.github.com>
2024-12-06 15:58:11 +0800

    Update 01.step01.index_align.sh

--- 01.step01.index_align.sh
+++ 01.step01.index_align.sh
@@ -8,7 +8,7 @@ export Contigs=$3
 HiC_R1=$4
 HiC_R2=$5
 
-samtools faidx ${EntityID}.fa
+samtools faidx ${EntityID}.fa > 01.step01.index_align.sh.log 2>&1
 chromap -i -r ${EntityID}.fa -o ${EntityID}.index 2>&1 | perl -ne '(/number of bases: (\d+)\.$/) && (print "assembly $1\n")'> ${EntityID}.chrom.sizes
 
 echo "Index Done."
@@ -16,7 +16,8 @@ echo "Index Done."
 chromap --preset hic -r ${EntityID}.fa -x ${EntityID}.index \
 -t 20 --SAM -o aligned.sam \
 -1 ${HiC_R1} \
--2 ${HiC_R2}
+-2 ${HiC_R2} \
+>> 01.step01.index_align.sh.log 2>&1
 
 echo "Alilgn Done."
 
@@ -26,8 +27,8 @@ samtools view -@ ${Threads} -b -F 4 -F 2048 aligned.bam -o aligned.pair.bam
 echo "Filter Done."
 
 export PATH=/opt/software/miniconda3/envs/java/bin:$PATH
-java -jar /opt/software/picard/picard.jar MarkDuplicates I=aligned.pair.bam O=${EntityID}.hicaln.dedup.bam REMOVE_DUPLICATES=true M=dedup_metrics.txt
-
+java -jar /opt/software/picard/picard.jar MarkDuplicates I=aligned.pair.bam O=${EntityID}.hicaln.dedup.bam REMOVE_DUPLICATES=true M=dedup_metrics.txt \
+>> 01.step01.index_align.sh.log 2>&1
 echo "Dedup Done."
 
 echo "Successful."
